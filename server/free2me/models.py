946033c2a5fe329fca4372194e5f97223875694c
Gustavo Walbon <glfwalbon@fitec.org.br>
2012-06-27 15:25:28 -0300

    New features into models
    - Modified method __unicode__.

--- server/free2me/models.py
+++ server/free2me/models.py
@@ -4,8 +4,14 @@ from django.db import models
 class Users(models.Model):
     name = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return self.name
+
 class Resource(models.Model):
     name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
 
 class Using(models.Model):
     user = models.ForeignKey(Users)
@@ -16,9 +22,11 @@ class Waiting(models.Model):
     resource = models.ForeignKey(Resource)
     time = models.DateTimeField(auto_now_add=True,)
 
+    def __unicode__(self):
+        return self.time
 
 class Relationship(models.Model):
     user = models.ForeignKey(Users)
     resource = models.ForeignKey(Resource)
-    
-    
+
+
