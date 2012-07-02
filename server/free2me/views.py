6ac74ba777c7f539129f6a65cacb44642ba2f2ce
Gustavo Walbon <gustavowalbon@gmail.com>
2012-07-02 00:25:58 -0300

    New features
    - Not finished!
    - TODO: Functions to add relationship into queue and others.

--- server/free2me/views.py
+++ server/free2me/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404
 from server.free2me.models import User,Resource,Relationship,Using,Waiting
+from django.http import HttpResponseRedirect
 
 def index(request):
     users = User.objects.all()
@@ -16,6 +17,10 @@ def resource(request,resource_name):
     relationship = get_list_or_404(Relationship, resource=resource_found)
     return render_to_response('./resource.html',{'resource':resource_found,'relationship':relationship})
 
+def resources(request):
+    resources = get_list_or_404(Resource)
+    return render_to_response('./resources.html',{'resources':resources})
+
 def using(request):
     using = Using.objects.all()
     return render_to_response(  './using.html', {'using':using})
@@ -23,4 +28,21 @@ def using(request):
 def waiting(request):
     waiting = Waiting.objects.all()
     return render_to_response(  './waiting.html', {'waiting':waiting})
+
+def pop_use(request):
+    try:
+        use = get_object_or_404(Using, name=request.POST['user'])
+    except (KeyError, Using.DoesNotExist):
+        return render_to_response('./NOTFOUND.html',{'item':"USER"})
+
+    try:
+        resource = get_object_or_404(Resource, name=request.POST['resource'])
+    except (KeyError, Resource.DoesNotExist):
+        return render_to_response('./NOTFOUND.html',{'item':"RESOURCE"})
+
+    if (use.resource.name == resource.name):
+        # It is necessary complete this step, adding tasks into queue.
+        return render_to_response('./use_pop.html',{'use':use})
+    else:
+        return render_to_response('./NOTFOUND.html',{'item':"Connection between datas"})
 
--- server/urls.py
+++ server/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,6 +10,11 @@ urlpatterns = patterns('server.free2me.views',
     (r'^user/(?P<user_name>\w+)/$', 'user'),
     (r'^resource/(?P<resource_name>\w+)/$', 'resource'),
     (r'^using/$', 'using'),
-    (r'^waiting/$', 'waiting')
-)
+    (r'^waiting/$', 'waiting'),
+    (r'^resources/$','resources'),
+    (r'^pop_use/$','pop_use'),
+    (r'^pop_wait/$','pop_wait'),
+    (r'^push_use/$','push_use'),
+    (r'^push_wait/$','push_wait')
+    )
 
